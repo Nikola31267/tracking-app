01d181e82477bfa20f33726d6e9885003ac3ed94
Nikola31267 <nikola.minchev13@gmail.com>
2025-10-28 10:35:17 +0200

    trying to fix axios instance

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8000/api/"
-      : "https://pixeltrackapi.startgrid.xyz/api/",
+      : "https://trackingapp-api.vercel.app/api/",
   withCredentials: true,
   headers: {
     "Access-Control-Allow-Origin": "http://localhost:3000",
